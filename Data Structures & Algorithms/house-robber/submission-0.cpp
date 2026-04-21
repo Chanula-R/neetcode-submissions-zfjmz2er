a4e5f769f8909fa783ef382f43da84151784fe54
Chanula-R <unichanula@gmail.com>
2026-04-21 09:10:17 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for (int num : nums)
+        {
+            int tmp = max(num+rob1,rob2);
+            rob1 = rob2;
+            rob2 = tmp;
+        }
+        return rob2;
+    }
+};
